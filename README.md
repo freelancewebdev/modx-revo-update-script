312422d352c892afad09ad7bca426e701e405ca0
Joe Molloy <molloyjoe@gmail.com>
2015-09-13 23:42:11 +0100

    Add licensing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # modx-revo-update-script
 A very rough MODX Revo upgrade script that I use for my own sites.
+
+This is licensed under the GPl V3 for anyone foolish enough to use it.
--- modx-upgrade.php
+++ modx-upgrade.php
@@ -1,4 +1,12 @@
 <?php
+/*
+
+Licensed under the GPL V3 
+
+@ Joe Molloy (info@hyper-typer.com) - http://twitter.com/freelancewebdev
+
+*/
+
 require_once 'config.core.php';
 require_once MODX_CORE_PATH.'model/modx/modx.class.php';
 $modx = new modX();
